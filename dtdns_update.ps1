9258aa79a902c19174e2d5c449f5242bf839118c
jeremybeuchot <jeremy1000@free.fr>
2017-02-16 17:51:36 +0100

    Add a loop try in case of http request error

--- dtdns_update.ps1
+++ dtdns_update.ps1
@@ -1,23 +1,33 @@
-#Enter your hostname and your account $password
-#Hostname must have the subdomain.domain.com. Ex : jeremybeuchot.dtdns.net
-$hostname = ""
+#Enter your hostname and your account password
+#Hostname must have the subdomain.domain.com format. Ex : jeremybeuchot.dtdns.net
+$hostname = ".dtdns.net"
 $password = ""
+$doRequest = $true
 
-#Get a page with your current IP
-$myIP = Invoke-WebRequest "https://domains.google.com/checkip"
-Write-Host "my ip is " $myIP
+do {
+    try {
+        #Get a page with your current IP
+        $myIP = Invoke-WebRequest "https://domains.google.com/checkip"
+        Write-Host "my ip is " $myIP
 
-#Make sure we got a IP back in the response
-If ($myIP.RawContent -match "(?:[0-9]{1,3}.){3}[0-9]{1,3}")
-{
-    #Build up the URL
-    $url = "http://www.dtdns.com/api/autodns.cfm?id={0}&pw={1}&client=dtdnsbash&ip={2}" -f $hostname, $password, $myIP
+        #Make sure we got a IP back in the response
+        If ($myIP.RawContent -match "(?:[0-9]{1,3}.){3}[0-9]{1,3}")
+        {
+            #Build up the URL
+            $url = "http://www.dtdns.com/api/autodns.cfm?id={0}&pw={1}&client=dtdnsbash&ip={2}" -f $hostname, $password, $myIP
 
-    #Invoke the URL
-    $response = Invoke-WebRequest -Uri $url
-    #content display the success or error message
-    Write-Host $response.Content
-}
-Else {
-    Write-Host "IP format is wrong"
-}
+            #Invoke the URL
+            $response = Invoke-WebRequest -Uri $url
+            #content display the success or error message
+            Write-Host $response.Content
+            $doRequest = $false
+        }
+        Else {
+            Write-Host "IP format is wrong"
+        }
+    }
+    catch {
+            Write-Host "Could not update dtdns, retrying in 10 seconds"            
+            Start-Sleep -Seconds 10
+        }
+} while ($doRequest)
